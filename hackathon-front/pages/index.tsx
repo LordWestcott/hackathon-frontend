232cc9e8895e82cda39f249be8b5652aebedfc29
Catherine McDonough <catherine.mcdonough@and.digital>
2022-12-14 12:38:17 +0000

    initial landing page

--- hackathon-front/pages/index.tsx
+++ hackathon-front/pages/index.tsx
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { LandingPage } from './components/LandingPage'
 
 export default function Home() {
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <LandingPage/>
     </>
   )
 }
