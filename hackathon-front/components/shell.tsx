21bdab0c2ed8e554cded9e78a30e611ce87ecc93
LordWestcott <ollyfilmer101@gmail.com>
2022-12-15 11:36:55 +0000

    Updated user icon to fit theme

--- hackathon-front/components/shell.tsx
+++ hackathon-front/components/shell.tsx
@@ -5,7 +5,7 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 
-import { FcHome, FcDonate, FcTimeline, FcRating } from "react-icons/fc";
+import { FcHome, FcDonate, FcTimeline, FcRating, FcBusinessman } from "react-icons/fc";
 import { useRouter } from 'next/router'
 
 //sidebar navigation options
@@ -183,9 +183,9 @@ export default function Shell({ title, children }: any) {
                   <div>
                     <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:dono-orange focus:ring-offset-2">
                       <span className="sr-only">Open user menu</span>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#00CD9C" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                      </svg>
+                      <div className='h-10 w-10'>
+                        <FcBusinessman className='h-full w-full'/>
+                      </div>
                     </Menu.Button>
                   </div>
                   <Transition
